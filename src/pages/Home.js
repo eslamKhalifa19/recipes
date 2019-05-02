4c7ab7f07558a6157db73ea42c7d6acb7614c6cd
eslam <eslamkhalefa19@gmail.com>
2019-05-02 21:27:51 +0200

    Header Component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,18 @@
 import React from "react";
+import Header from "../components/Header";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <h4>Hello From Home</h4>;
+  return (
+    <Header title="amazing recipes">
+      <Link
+        to="recipes"
+        className="text-uppercase btn btn-secondary btn-lg mt-3"
+      >
+        search recipes
+      </Link>
+    </Header>
+  );
 };
 
 export default Home;
