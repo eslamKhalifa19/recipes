eb6c0f2132841e6dcb96174ec779c1cc50eb053d
eslam <eslamkhalefa19@gmail.com>
2019-05-03 13:59:00 +0200

    Default page

--- src/pages/Default.js
+++ src/pages/Default.js
@@ -1,7 +1,16 @@
 import React from "react";
+import Header from "../components/Header";
+import { Link } from "react-router-dom";
 
 const Default = () => {
-  return <h4>Hello From ErrorPage</h4>;
+  return (
+    <Header title="404" styleClass="default-hero">
+      <h2 className="text-light text-uppercase">you are in the wrong place</h2>
+      <Link to="/" className="text-uppercase btn btn-secondary btn-lg mt-3">
+        return home
+      </Link>
+    </Header>
+  );
 };
 
 export default Default;
