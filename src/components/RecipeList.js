6471ee3070a72bc9b413088947ac8f75f4f0e9a3
eslam <eslamkhalefa19@gmail.com>
2019-05-03 17:22:04 +0200

    RecipeList Component

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,12 +1,22 @@
 import React from "react";
 import Recipe from "./Recipe";
 
-const RecipeList = () => {
+const RecipeList = ({ recipes }) => {
   return (
-    <div>
-      hello from RecipeList
-      <Recipe />
-    </div>
+    <>
+      <div className="container py-5">
+        <div className="row">
+          <div className="col-10 mx-auto col-md-6 text-center text-uppercase mb-3">
+            <h1 className="text-slanted">recipe list</h1>
+          </div>
+        </div>
+        <div className="row">
+          {recipes.map(recipe => (
+            <Recipe key={recipe.recipe_id} recipe={recipe} />
+          ))}
+        </div>
+      </div>
+    </>
   );
 };
 
